f666d33afced7479dc3f328ce1125cd1f8bc0323
mdismail645221 <mdismail645221@gmail.com>
2022-11-25 02:23:42 +0600

    all category api added

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const jwt = require('jsonwebtoken');
 // console.log(stripe)
 
 
+// pass ==> zVNc0wb50ZWu38Za
+//  user= bikroyBazar645221
+
+
+
 // middleware
 app.use(cors());
 // app.use(express.static("public"));
@@ -18,12 +23,63 @@ app.use(express.json());
 
 
 // start ====>
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send('assignment 12 is running.')
 })
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `${process.env.MONGODB_URL}`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const categoriesCollection = client.db("bikroyBazar645221").collection("allCategories")
+        const huaweiCollection = client.db("bikroyBazar645221").collection("huaweiPhones");
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const filter = { _id: ObjectId(id) };
+            const result = await categoriesCollection.findOne(filter);
+            res.send(result)
+        })
+
+        // Modify the add Location
+        // app.get('/addLocation', async(req, res)=> {
+        //   const query = {
+        //     location: "Dhaka"
+        //   };
+        //   const options = {upsert: true};
+        //   const updateDoc = {
+        //     $set: {
+        //       Location: "Dhaka"
+        //     }
+        //   }
+        //     const result = await categoriesCollection.updateMany(query, updateDoc, options);
+        //   res.send(result)
+        // })
+
+
+    }
+    catch {
+        (error) => {
+            console.log(error)
+        }
+    }
+
+}
+run().catch((error) => console.log(error));
+
+
 
 
 
 app.listen(port, () => {
     console.log('assignment 12', port)
-  })
+})
